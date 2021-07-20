cf76099dcc4497d1b88ca25042dbb17b5eee091b
杨朋飞 <pengfei.yang@maxim-group.com>
2021-07-20 17:52:17 +0800

    no message

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Amplify from 'aws-amplify';
-import config from './aws-exports';
+import config from './cc';
 Amplify.configure(config);
 ReactDOM.render(
   <React.StrictMode>
